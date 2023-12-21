7516171d1cce609e24db9d94a9b0a1b6b18a15cf
f.guglielmucci <148483575+fg-44@users.noreply.github.com>
2023-12-21 16:18:13 +0100

    UPDATE 3

--- Epilibrary3/src/components/CommentArea.jsx
+++ Epilibrary3/src/components/CommentArea.jsx
@@ -1,14 +1,39 @@
-import React, { useState } from 'react';
+import React, { UseEffect } from 'react';
+import { useState } from 'react';
 
-const CommentArea = ({ comment }) => {
+const CommentArea = ({ book }) => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    const fetchComments = async () => {
+      const response = await fetch(`/comment/fetch/${book.id}`);
+      const comments = await response.json();
+      setComments(comments);
+    };
+
+    fetchComments();
+  }, [book, setComments]);
+
   return (
-    <div className="comment-area">
-      <p key={comment.id}>ID:{comment.id}</p>
-      <p>Text: {comment.text}</p>
-      <p>Author: {comment.author}</p>
-      <p>Date: {comment.date}</p>
+    <div>
+      <h3>Comments for this Book</h3>
+      {comments.length === 0 ? (
+        <p>There are no comments for this book yet.</p>
+      ) : (
+        <CommentsList comments={comments} />
+      )}
     </div>
   );
 };
+
+const CommentsList = ({ comments }) => {
+  return (
+    <ul>
+      {comments.map(comment => (
+        <li key={comment.id}>{comment.text}</li>
+      ))}
+    </ul>
+  )
+}
 
 export default CommentArea;
